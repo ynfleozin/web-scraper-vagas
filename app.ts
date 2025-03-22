fa9d728d0a01cb2eb0d8a1e30c518372a1d6f118
ynfleozin <leonardoalvarengam@gmail.com>
2025-03-22 15:24:06 -0300

    feat: add expertise filtering to /vagas endpoint and install AWS Lambda types

--- app.ts
+++ app.ts
@@ -1,43 +1,57 @@
-const puppeteer = require('puppeteer');
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+const puppeteer = require("puppeteer");
 
 type Job = {
-    title: string;
-    company: string;
-    skills: string[];
-}
+  title: string;
+  company: string;
+  skills: string[];
+};
 
-export async function run(){
-    const url = "https://programathor.com.br/jobs";
+export async function run(event: APIGatewayProxyEvent) {
+  const expertise = event.pathParameters?.expertise || "";
 
-    const browser = await puppeteer.launch();
+  const url = `https://programathor.com.br/jobs${
+    expertise ? `?expertise=${encodeURIComponent(expertise)}` : ""
+  }`;
 
-    const page = await browser.newPage();
-    await page.goto(url)
+  const browser = await puppeteer.launch();
 
-    const content = await page.evaluate(() => {
-        const blocks = document.querySelectorAll(".cell-list");
-        const array = [...blocks].slice(0,4);
+  const page = await browser.newPage();
+  await page.goto(url);
 
-        return array.map((item) => {
-            const title = item.querySelector(".cell-list-content h3.text-24.line-height-30")?.textContent?.trim();
-            const company = item.querySelector(".cell-list-content-icon .fa-briefcase")?.parentElement?.textContent?.trim();
+  const content = await page.evaluate(() => {
+    const blocks = document.querySelectorAll(".cell-list");
+    const array = [...blocks].slice(0, 4);
 
-            const skillElements = item.querySelectorAll(".cell-list-content .tag-list.background-gray");
-            const skills = Array.from(skillElements).map((skill) => skill.textContent?.trim());
+    return array.map((item) => {
+      const title = item
+        .querySelector(".cell-list-content h3.text-24.line-height-30")
+        ?.textContent?.trim();
+      const company = item
+        .querySelector(".cell-list-content-icon .fa-briefcase")
+        ?.parentElement?.textContent?.trim();
 
-            return {
-                title,
-                company,
-                skills,
-            };
-        });
+      const skillElements = item.querySelectorAll(
+        ".cell-list-content .tag-list.background-gray"
+      );
+      const skills = Array.from(skillElements).map((skill) =>
+        skill.textContent?.trim()
+      );
+
+      return {
+        title,
+        company,
+        skills,
+      };
     });
+  });
 
-    await browser.close();
+  await browser.close();
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({content}),
-        headers: {"Content-Type": "application/json"}
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ content }),
+    headers: { "Content-Type": "application/json" },
+  };
 }
